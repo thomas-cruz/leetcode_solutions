488d09c575acef20356a4f23214164155613af61
Thomas Cruz <50308862+thomas-cruz@users.noreply.github.com>
2023-10-27 23:53:24 +0800

    Added further explanation to linked-list-cycle.js

--- linked-list-cycle.js
+++ linked-list-cycle.js
@@ -32,6 +32,19 @@ We just need to follow the strategy above - make a slower pointer and a faster p
 
     if the fast pointer catch up with slow pointer, then it's a circular linked list
     if the fast pointer get to the end, then it's not a circular linked list
+
+
+
+Floyd’s Cycle Finding Algorithm
+Floyd’s cycle finding algorithm or Hare-Tortoise algorithm is a pointer algorithm that uses only two pointers, moving through the sequence at different speeds. This algorithm is used to find a loop in a linked list. It uses two pointers one moving twice as fast as the other one. The faster one is called the fast pointer and the other one is called the slow pointer.
+
+How Does Floyd’s Cycle Finding Algorithm Works?
+
+While traversing the linked list one of these things will occur-
+
+    The Fast pointer may reach the end (NULL) this shows that there is no loop in the linked list.
+    The Fast pointer again catches the slow pointer at some time therefore a loop exists in the linked list.
+
 */
 
 /**
